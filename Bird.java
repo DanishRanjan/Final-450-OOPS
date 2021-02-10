455e9b533c1b580da8aa14ea273be12b20274020
Danish Ranjan <ranjandanish@gmail.com>
2021-02-10 18:06:02 +0530

    Introduction to Inheritance

---
+++ Bird.java
@@ -0,0 +1,25 @@
+//INHERITANCE
+//main advantages of inheritance is code reusability
+//we can use achieve polymorphism using inheritance
+//disadvantage is : both class tightly coupled
+//Types of Inheritance in java
+//1. Single Inheritance.
+//2. Multilevel Inheritance.
+//3. Hierarchical Inheritance.
+//4. Multiple Inheritance. (not applicable in java)
+//5. Hybrid Inheritance (also known as Virtual Inheritance) (not applicable in java)
+
+
+
+public class Bird extends Sparrow {	
+	public static void main(String args[]) {
+		Sparrow sp = new Sparrow();
+		sp.eat();
+	}
+}
+
+class Sparrow  {
+	void eat() {
+		System.out.println("i am eating");
+	}
+}
